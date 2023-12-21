3db48ee2580a4d0e895ff3ccc4b4d7f48c1f66f3
Shane <palmerc@MBP.home>
2023-12-21 09:21:38 -0800

    started boilerplat for emitting bytecode

--- compiler.h
+++ compiler.h
@@ -1,6 +1,9 @@
+#include <stdbool.h>
 #ifndef mu_compiler_h
 #define mu_compiler_h
 
-void compile(const char* source);
+#include "vm.h"
+
+bool compile(const char* source, Chunk* chunk);
 
 #endif
--- scanner.c
+++ scanner.c
@@ -26,6 +26,10 @@ Token scanToken() { //used in compiler.c
   }
   char rune = advance();
 
+  if (isAlpha(rune)) {
+    return identifier();
+  }
+
   if (isDigit(rune)) {
     return numberize();
   }
@@ -46,7 +50,7 @@ Token scanToken() { //used in compiler.c
     case '*' : return tokenize(S_STAR);
     case '/' : return tokenize(S_SLASH);
     // double character
-    case '!' : return tokenize(match('~') ? D_NOT_EQUAL : S_BANG);
+    case '!' : return tokenize(match('~') ? D_BANG_TILDE : S_BANG);
     case '=' : return tokenize(match('=') ? D_EQUAL : S_EQUAL);
     case '<' : return tokenize(match('=') ? D_LESS_EQUAL : S_LESS);
     case '>' : return tokenize(match('=') ? D_GREATER_EQUAL : S_GREATER);
@@ -80,6 +84,11 @@ static bool match(char expected) {
   }
   scanner.current += 1;
   return true;
+}
+static bool isAlpha(char rune) {
+  return (rune >= 'a' && rune <= 'z') ||
+         (rune >= 'A' && rune <= 'Z') ||
+          rune == '_';
 }
 static bool isDigit(char rune) {
   return rune >= '0' && rune <= '9';
@@ -138,6 +147,61 @@ static void handleWhitespace() {
         return;
     }
   }
+}
+
+static TokenType testKeyword(int start, int length, const char* remaining, TokenType token) {
+  if (scanner.current - scanner.start == start + length
+      && memcmp(scanner.start + start, remaining, length) == 0) {
+        return token;
+  }
+  return L_IDENTIFIER;
+}
+/*
+case 'f':
+      if (scanner.current - scanner.start > 1) {
+        switch (scanner.start[1]) {
+          case 'a': return checkKeyword(2, 3, "lse", TOKEN_FALSE);
+          case 'o': return checkKeyword(2, 1, "r", TOKEN_FOR);
+          case 'u': return checkKeyword(2, 1, "n", TOKEN_FUN);
+        }
+      }
+      break;
+*/
+static TokenType KeywordOrLiteral() {
+  switch (scanner.start[0]) {
+    case 'a' : return testKeyword(1, 2, "nd", K_AND);
+    case 'b' : return testKeyword(1, 4, "uild", K_BUILD);
+    case 'd' : return testKeyword(1, 5, "efine", K_DEFINE);
+    case 'e' : return (scanner.current - scanner.start > 1 && scanner.start[1] == 'n') 
+      ? testKeyword(1, 2, "nd", K_END) 
+      : testKeyword(1, 3, "lse", K_ELSE);
+    case 'f' : return testKeyword(1, 4, "alse", K_FALSE);
+    case 'i' : return testKeyword(1, 1, "f", K_IF);
+    case 'j' : return testKeyword(1, 3, "oin", K_JOIN);
+    case 'o' : return testKeyword(1, 1, "r", K_OR);
+    case 'r' : return testKeyword(1, 5, "eturn", K_RETURN);
+    case 's' : return testKeyword(1, 3, "elf", K_SELF);
+    case 't' : return testKeyword(1, 4, "rue", K_TRUE);
+    case 'u' : if (scanner.current - scanner.start > 1 && scanner.start[1] == 'n') {
+      switch (scanner.start[2]) {
+        case 'l' : return testKeyword(1, 3, "ess", K_UNLESS);
+        case 't' : return testKeyword(1, 2, "il", K_UNTIL);
+      }
+    }
+    case 'v' : return testKeyword(1, 3, "oid", K_VOID);
+    case 'w' : return testKeyword(1, 3, "hen", K_WHEN);
+    case 'x' : return testKeyword(1, 2, "or", K_XOR);
+    case 'y' : return testKeyword(1, 4, "ield", K_YIELD);
+    case 'z' : return testKeyword(1, 3, "one", K_ZONE);
+  }
+  return L_IDENTIFIER;
+}
+
+static Token identifier() {
+  while (isAlpha(peek()) || isDigit(peek())) {
+    advance();
+  }
+  return tokenize(KeywordOrLiteral());
 }
 
 static Token string() {
