d088e70a9baff65f8a8b781af02d9e7bbeb1fbd3
Shane <palmerc@MBP.home>
2024-01-16 21:09:58 -0800

    testing for highlighting

--- sample.java
+++ sample.java
@@ -2,17 +2,17 @@ print "===Mu sample start===";
 
 print " --Not Operator";
 print 2 = 1;
-print !(2 < 5);
-print !(3 = 2 + 1);
-print !(3 > 3 * 2);
-print !(2 = 1);
+print!(2 < 5);
+print!(3 = 2 + 1);
+print!(3 > 3 * 2);
+print!(2 = 1);
 
 print " --While Loop";
 let #count := 1;
 
-while #count < 100 ? 
-    print #count; 
-    #count := #count * 2; // not getting from globals
+while #count < 100 ?
+    print #count;
+#count := #count * 2; 
 ,,
 
 print " --Until Loop";
@@ -20,42 +20,42 @@ print " --Until Loop";
 
 until #count > 100 ?
     print #count;
-    #count := #count * 3;
+#count := #count * 3;
 ,,
 
 print " --If Clause";
-if 0 > -5 ? 
+if 0 > -5 ?
     print "yes, zero is greater than negative five"; 
 ,,
 
-if 0 > 10 ? 
-    print "yes zero is greater than ten"; 
+if 0 > 10 ?
+    print "yes zero is greater than ten";
 else 
     print "no, zero is not greater than ten";
 // ,,
 
-if false ? 
-    print "one"; 
-else if false ? 
-    print "two"; 
+if false ?
+    print "one";
+else if false ?
+    print "two";
 else 
     print "three";
 // ,,
 
 print " --Unless Clause";
-let x : 5;
-unless x = 4 ? 
+let x: 5;
+unless x = 4 ?
     print "good"; 
 ,,
 
-unless x = 5 ? 
+unless x = 5 ?
     print "not five"; 
 else print "five"; // todo always prints...
 
 print " --String Concatenation";
-let name : "steve";
-let last : " poe";
-let full_name : name + last;
+let name: "steve";
+let last: " poe";
+let full_name: name + last;
 print full_name;
 
 print " --functions";
